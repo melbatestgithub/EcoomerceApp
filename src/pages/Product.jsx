8b8f2a9c806b624301ef5f0e199b80a73ef0766d
melakuzeleke <melakuzeleke443@gmail.com>
2024-06-25 19:41:54 +0300

    commit

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,4 +1,4 @@
-import { Add, Remove } from "@material-ui/icons";
+import { Add, Remove } from "@mui/icons-material";
 import styled from "styled-components";
 import { useState, useEffect } from "react";
 import Navbar from "../components/Navbar";
